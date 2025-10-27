c5371b2429e0d233c9ab800c59a0ef745ca02aaf
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-10-27 13:03:54 +0530

    objects and constructers

---
+++ src/Objects_Constructers.java
@@ -0,0 +1,17 @@
+public class Objects_Constructers {
+
+
+    //defining class and objects
+    public class rectangle{
+        double length;
+        double breadth;
+        double calparameter(){
+            return (2*length)+(2*breadth);
+            }
+        double calarea(){
+            return length * breadth;
+        }
+
+
+    }
+}
